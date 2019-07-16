e1ba7c7bdbbfb4d73afb6b8c728757f1696af812
Tng Renjie <tngrenjie@yahoo.com.sg>
2019-07-16 23:09:48 +0800

    Second commit because github screwed up css folder

---
+++ README.md
@@ -0,0 +1,3 @@
+# listMe
+
+Make a way to add more to a list with a pre generated check box. When check box is ticked it will move the item down to the bottom of the list alphabetically
---
+++ index.js
@@ -0,0 +1,36 @@
+function openForm() {
+	document.getElementById('addTask').style.display = 'block';
+}
+
+function closeForm() {
+	document.getElementById('addTask').style.display = 'none';
+	let input = document.getElementById('task').value;
+	document.getElementById('task').value = '';
+	addToList(input);
+}
+
+let fragment = document.createDocumentFragment();
+let x = 1;
+
+function addToList(input) {
+	console.log(x);
+	let para = document.createElement('p');
+	para.id = x;
+	para.appendChild(document.createTextNode(input));
+	let element = document.getElementById('listOfTasks');
+	element.appendChild(para);
+	addCheckBox(x);
+	x++;
+}
+function addCheckBox(id) {
+	let checkBox = document.createElement('a');
+	checkBox.className = 'button is-rounded is-small';
+	document.getElementById(id).appendChild(checkBox);
+}
+function deleteMe() {
+	//test button
+	// var parent = document.getElementById('listofTasks');
+	// var child = document.getElementById('joke');
+	// parent.removeChild(child);
+	document.getElementById('1').outerHTML = '';
+}
